0d0392849e4c3c2ecd86423e8bbdbec90de89eb7
iblais <itsblais@gmail.com>
2026-03-04 16:25:43 -0500

    Add TikTok verification as API route

---
+++ src/app/tiktokx6bKtJgP0Tbnf3ANrHmFIUXWrWyWtDUx.txt/route.ts
@@ -0,0 +1,5 @@
+export async function GET() {
+  return new Response('tiktok-developers-site-verification=x6bKtJgP0Tbnf3ANrHmFIUXWrWyWtDUx', {
+    headers: { 'Content-Type': 'text/plain' },
+  });
+}
